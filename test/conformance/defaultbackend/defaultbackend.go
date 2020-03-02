3e58562702f9c6689230487e02cb58cdf1a51c04
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2020-03-02 17:42:22 -0300

    Start implementation of default backend feature

--- test/conformance/defaultbackend/defaultbackend.go
+++ test/conformance/defaultbackend/defaultbackend.go
@@ -1,20 +1,24 @@
 package defaultbackend
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/cucumber/godog"
 	"github.com/cucumber/godog/gherkin"
+	v1beta1 "k8s.io/api/networking/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	clientset "k8s.io/client-go/kubernetes"
 
+	"github.com/aledbf/ingress-conformance-bdd/test/state"
 	"github.com/aledbf/ingress-conformance-bdd/test/utils"
 )
 
 type feature struct {
-	state  *utils.State
+	state *state.Feature
+
 	client *clientset.Clientset
-
-	namespace string
-
-	ingressIPOrFQDN string
 }
 
 func (f *feature) aNewRandomNamespace() error {
@@ -23,16 +27,62 @@ func (f *feature) aNewRandomNamespace() error {
 		return err
 	}
 
-	f.namespace = ns
+	f.state.Namespace = ns
 	return nil
+}
+
+func (f *feature) anIngressIsCreatedWithHostAndNoBackend(host string) error {
+	ingSpec := &v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "defaultbackend",
+			Namespace: f.state.Namespace,
+		},
+		Spec: v1beta1.IngressSpec{
+			Rules: []v1beta1.IngressRule{
+				{
+					Host: host,
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Path: "/",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	f.state.SetIngress(ingSpec)
+	return nil
+}
+
+func (f *feature) ingressCreationrrorMessageContains(expected string) error {
+	_, err := utils.CreateIngress(f.client, f.state.GetIngress())
+	if err == nil {
+		return fmt.Errorf("Expected an error creating an ingress without backend serviceName")
+	}
+
+	if strings.Contains(err.Error(), expected) {
+		return nil
+	}
+
+	return fmt.Errorf("Expected an error containing %v but returned %v", expected, err.Error())
 }
 
 func (f *feature) ingressStatusIPOrFQDN() error {
-	f.ingressIPOrFQDN = "1.1.1.1"
-	return nil
-}
+	if f.state.GetIngress() == nil {
+		return fmt.Errorf("Feature without Ingress associated")
+	}
 
-func (f *feature) anIngressIsCreatedWithHostAndNoBackend(arg1 string) error {
+	address, err := utils.WaitForIngressAddress(f.client, f.state.Namespace, f.state.GetIngress().GetName(), "", utils.WaitForIngressAddressTimeout)
+	if err != nil {
+		return err
+	}
+
+	f.state.SetStatusAddress(address)
 	return nil
 }
 
@@ -52,12 +102,42 @@ func (f *feature) headerIs(arg1, arg2 string) error {
 	return nil
 }
 
-func (f *feature) anIngressIsCreatedWithFoobarHostWithInvalidBackend() error {
-	return nil
-}
+func (f *feature) anIngressIsCreatedWithFoobarHostWithInvalidBackend(host string) error {
+	ingSpec := &v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "defaultbackend",
+			Namespace: f.state.Namespace,
+		},
+		Spec: v1beta1.IngressSpec{
+			Rules: []v1beta1.IngressRule{
+				{
+					Host: host,
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Path: "/",
+									Backend: v1beta1.IngressBackend{
+										ServiceName: "non-existing",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 
-func (f *feature) anIngressIsCreatedWithHostWithInvalidBackend(arg1 string) error {
-	return godog.ErrPending
+	var err error
+	ingSpec, err = utils.CreateIngress(f.client, ingSpec)
+	if err != nil {
+		return err
+	}
+
+	f.state.SetIngress(ingSpec)
+	return nil
 }
 
 func (f *feature) sendHTTPRequestWithPathAndMethodCheckingResponseStatusCodeIs(arg1 int, arg2 *gherkin.DataTable) error {
@@ -74,27 +154,23 @@ func FeatureContext(s *godog.Suite, c *clientset.Clientset) {
 	}
 
 	s.Step(`^a new random namespace$`, f.aNewRandomNamespace)
-	s.Step(`^an Ingress is created with host "([^"]*)" and no backend$`, f.anIngressIsCreatedWithHostAndNoBackend)
+	s.Step(`^creating an Ingress with host "([^"]*)" without backend serviceName$`, f.anIngressIsCreatedWithHostAndNoBackend)
+	s.Step(`^The error message contains "([^"]*)"$`, f.ingressCreationrrorMessageContains)
 	s.Step(`^The ingress status shows the IP address or FQDN where is exposed$`, f.ingressStatusIPOrFQDN)
 	s.Step(`^Header "([^"]*)" with value "([^"]*)"$`, f.headerWithValue)
 	s.Step(`^Send HTTP request with method "([^"]*)"$`, f.sendHTTPRequestWithMethod)
 	s.Step(`^Response status code is (\d+)$`, f.responseStatusCodeIs)
 	s.Step(`^Header "([^"]*)" is "([^"]*)"$`, f.headerIs)
-	s.Step(`^an Ingress is created with foo\.bar host with invalid backend$`, f.anIngressIsCreatedWithFoobarHostWithInvalidBackend)
-	s.Step(`^an Ingress is created with host "([^"]*)" with invalid backend$`, f.anIngressIsCreatedWithHostWithInvalidBackend)
+	s.Step(`^an Ingress is created with host "([^"]*)" with an invalid backend$`, f.anIngressIsCreatedWithFoobarHostWithInvalidBackend)
 	s.Step(`^Send HTTP request with <path> and <method> checking response status code is (\d+):$`, f.sendHTTPRequestWithPathAndMethodCheckingResponseStatusCodeIs)
 	s.Step(`^With path "([^"]*)"$`, f.withPath)
 
 	s.BeforeScenario(func(this interface{}) {
-		switch this.(type) {
-		case *gherkin.Scenario:
-			f.state = utils.NewState()
-		}
+		f.state = state.New()
 	})
 
 	s.AfterScenario(func(interface{}, error) {
 		// delete namespace an all the content
-		utils.DeleteKubeNamespace(c, f.namespace)
-		f.namespace = ""
+		utils.DeleteKubeNamespace(c, f.state.Namespace)
 	})
 }

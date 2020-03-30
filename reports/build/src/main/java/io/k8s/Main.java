0700492119e8c5ee966ab4c7346929b9e21fdc24
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2020-03-30 14:20:23 -0300

    Update Java class to generate report from a list of files

--- reports/build/src/main/java/io/k8s/Main.java
+++ reports/build/src/main/java/io/k8s/Main.java
@@ -2,8 +2,12 @@ package io.k8s;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
@@ -12,38 +16,41 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        String outputDirectory = System.getenv("OUTPUT_DIRECTORY");
-        if (outputDirectory == null) {
-            outputDirectory = "/report-output";
-        }
-        File reportOutputDirectory = new File(outputDirectory);
+	public static void main(String[] args) throws IOException {
+		String outputDirectory = System.getenv("OUTPUT_DIRECTORY");
+		if (outputDirectory == null) {
+			outputDirectory = "/reports";
+		}
+		File reportOutputDirectory = new File(outputDirectory);
 
-        String inputJSON = System.getenv("INPUT_JSON");
-        if (inputJSON == null) {
-            inputJSON = "/input.json";
-        }
+		String inputFiles = System.getenv("INPUT_JSON_FILES");
+		if (inputFiles == null) {
+			throw new RuntimeException("Environment variable INPUT_JSON_FILES is not optional");
+		}
 
-        List<String> jsonFiles = new ArrayList<>();
-        jsonFiles.add(inputJSON);
+		Stream<Path> walk = Files.walk(Paths.get(inputFiles));
+		List<String> reports = walk.map(x -> x.toString()).filter(f -> f.endsWith("-report.json"))
+				.collect(Collectors.toList());	
+		
+		String buildNumber = "1";
+		String projectName = "Ingress Conformance Test";
 
-        String buildNumber = "1";
-        String projectName = "Ingress Conformance Test";
+		Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+		configuration.setBuildNumber(buildNumber);
+		configuration.addClassifications("Release", "1.19");
+		configuration.setSortingMethod(SortingMethod.NATURAL);
+		configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
 
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        configuration.setBuildNumber(buildNumber);
-        configuration.addClassifications("Release", "1.19");
-        configuration.setSortingMethod(SortingMethod.NATURAL);
-        configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+		String trendJSON = System.getenv("TREND_JSON");
+		if (trendJSON == null) {
+			trendJSON = "/reports/trends.json";
+		}
 
-        String trendJSON = System.getenv("TREND_JSON");
-        if (trendJSON == null) {
-            trendJSON = "/report-output/trends.json";
-        }
+		configuration.setTrendsStatsFile(new File(trendJSON));
 
-        configuration.setTrendsStatsFile(new File(trendJSON));
-
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
-        reportBuilder.generateReports();
-    }
+		ReportBuilder reportBuilder = new ReportBuilder(reports, configuration);
+		reportBuilder.generateReports();
+		
+		walk.close();
+	}
 }
